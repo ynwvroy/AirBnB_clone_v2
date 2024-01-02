ab013cfb5c8f7d1c882e3aee2e75963b2a58b115
Jonathan <jonathankyule2@gmail.com>
2024-01-01 18:42:25 -0800

    updates

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
-# install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+# sets up a web server for deployment of web_static.
 
-# Create necessary folders if they don't exist
-folders=("/data/" "/data/web_static/" "/data/web_static/releases/" "/data/web_static/shared/" "/data/web_static/releases/test/")
-for folder in "${folders[@]}"; do
-    if [ ! -d "$folder" ]; then
-        sudo mkdir -p "$folder"
-    fi
-done
+apt-get update
+apt-get install -y nginx
 
-# Create a fake HTML file for testing
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create symbolic link, delete and recreate if it already exists
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Give ownership to the ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $hostname;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://github.com/besthor;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-if [ -f "$config_file" ]; then
-    # Add alias to serve content from /data/web_static/current/
-    sudo sed -i '/server_name _;/a\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' "$config_file"
-    # Restart Nginx
-    sudo service nginx restart
-fi
-
-# Exit successfully
-exit 0
+service nginx restart
